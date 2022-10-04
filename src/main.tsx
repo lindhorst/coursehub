9df6e47c48cdecf83f51131f7e07d0b18385499e
lindhorst <p.korol3v@gmail.com>
2022-10-04 04:27:59 +0300

    Added fonts

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './styles/fonts.scss';
+import './styles/globals.scss';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+	<React.StrictMode>
+		<h1>Hello world</h1>
+	</React.StrictMode>
+);
